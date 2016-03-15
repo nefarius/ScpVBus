fdc16ab4c66316add9904c3c8d8d38f47597c841
Benjamin Höglinger <nefarius@dhmx.at>
2016-03-15 14:02:12 +0100

    Added missing header file Implemented function skeletons

--- XOutput/XOutput.cpp
+++ XOutput/XOutput.cpp
@@ -2,5 +2,48 @@
 //
 
 #include "stdafx.h"
+#include "XOutput.h"
 
+///-------------------------------------------------------------------------------------------------
+/// <summary>	Output set state. </summary>
+///
+/// <remarks>	Benjamin, 15.03.2016. </remarks>
+///
+/// <param name="dwUserIndex">	Zero-based index of the user. </param>
+/// <param name="pState">	  	[in,out] If non-null, the state. </param>
+///
+/// <returns>	A DWORD. </returns>
+///-------------------------------------------------------------------------------------------------
+DWORD XOutputSetState(DWORD dwUserIndex, XINPUT_STATE * pState)
+{
+	return 0;
+}
 
+///-------------------------------------------------------------------------------------------------
+/// <summary>	Output get state. </summary>
+///
+/// <remarks>	Benjamin, 15.03.2016. </remarks>
+///
+/// <param name="dwUserIndex">	Zero-based index of the user. </param>
+/// <param name="pVibration"> 	[in,out] If non-null, the vibration. </param>
+///
+/// <returns>	A DWORD. </returns>
+///-------------------------------------------------------------------------------------------------
+DWORD XOutputGetState(DWORD dwUserIndex, XINPUT_VIBRATION * pVibration)
+{
+	return 0;
+}
+
+///-------------------------------------------------------------------------------------------------
+/// <summary>	Output get real user index. </summary>
+///
+/// <remarks>	Benjamin, 15.03.2016. </remarks>
+///
+/// <param name="dwUserIndex">	Zero-based index of the user. </param>
+///
+/// <returns>	A DWORD. </returns>
+///-------------------------------------------------------------------------------------------------
+DWORD XOutputGetRealUserIndex(DWORD dwUserIndex)
+{
+	return 0;
+}
---
+++ XOutput/XOutput.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "stdafx.h"
+#include <Xinput.h>
+
+#ifndef DLL_EXPORT
+  #if XOUTPUT_EXPORTS
+    #define DLL_EXPORT __declspec(dllexport)
+  #else
+    #define DLL_EXPORT __declspec(dllimport)
+  #endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {  // only need to export C interface if
+			  // used by C++ source code
+#endif
+
+	DLL_EXPORT DWORD XOutputSetState(
+		_In_  DWORD        dwUserIndex,
+		_Out_ XINPUT_STATE *pState
+		);
+
+	DLL_EXPORT DWORD XOutputGetState(
+		_In_    DWORD            dwUserIndex,
+		_Inout_ XINPUT_VIBRATION *pVibration
+		);
+
+	DLL_EXPORT DWORD XOutputGetRealUserIndex(
+		_In_    DWORD            dwUserIndex
+		);
+
+#ifdef __cplusplus
+}
+#endif
